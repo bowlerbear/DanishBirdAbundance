1d6d686a4697022007e5e85690b01d9ea46a44d9
CTC <callaghan.corey.t@gmail.com>
2022-05-20 11:24:03 +0200

    corey playing around with the model of ESW using traits

--- 02_ESW_analysis.R
+++ 02_ESW_analysis.R
@@ -167,6 +167,183 @@ plot_grid(g1,g3,
 summary(lm(ESW ~ log(Mass) + ForStrat.ground + Habitat + 
              Trophic.Level + Primary.Lifestyle + Trophic.Niche,
             data=traitsDF))
+
+
+#####################################
+#####################################
+####### SOME STUFF FROM COREY
+#####################################
+library(broom)
+library(tidyverse)
+
+# do a leave-one-out type thing
+# where each species is left out at a time and
+# then their ESW is predicted
+leave_one_out_function <- function(species_name){
+  
+  dat_filtered <- traitsDF %>%
+    dplyr::filter(Species != species_name)
+  
+  # I removed "habitat from the model because it is tricky because
+  # there are two levels where only one species is that habitat
+  mod <- lm(ESW ~ log(Mass) + ForStrat.ground + 
+              Trophic.Level + Primary.Lifestyle + Trophic.Niche,
+            data=dat_filtered)
+  
+  new_dat <- traitsDF %>%
+    dplyr::filter(Species==species_name)
+  
+  predicted_ESW <- predict(mod, new_dat)
+  
+  loo_summary <- data.frame(Species=species_name,
+                            observed_ESW=new_dat$ESW,
+                            predicted_ESW=predicted_ESW)
+  
+  return(loo_summary)
+  
+}
+
+leave_one_out_results <- bind_rows(lapply(unique(traitsDF$Species), leave_one_out_function))
+
+ggplot(leave_one_out_results, aes(x=observed_ESW, y=predicted_ESW))+
+  geom_point()+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  xlab("Observed ESW")+
+  ylab("Predicted ESW")+
+  geom_smooth(method="lm")
+
+summary(lm(observed_ESW ~ predicted_ESW, data=leave_one_out_results))
+
+# but how many species can we leave out at a time and still get a good predictive accuracy of the model?
+random_sampling_function <- function(sample_size){
+  
+  boot_fun <- function(draw_number){
+    
+    dat_filtered <- traitsDF %>%
+      sample_n(nrow(traitsDF)-sample_size)
+    
+    mod <- lm(ESW ~ log(Mass) + ForStrat.ground +  
+                Trophic.Level + Primary.Lifestyle + Trophic.Niche,
+              data=dat_filtered)
+    
+    new_dat <- traitsDF %>%
+      dplyr::filter(!Species %in% dat_filtered$Species)
+    
+    predicted_ESW <- predict(mod, new_dat)
+    
+    loo_summary <- data.frame(Species=new_dat$Species,
+                              observed_ESW=new_dat$ESW,
+                              predicted_ESW=predicted_ESW) %>%
+      mutate(draw=draw_number)
+    
+    return(loo_summary)
+    
+  }
+  
+  boot_results <- bind_rows(lapply(c(1:50), boot_fun)) %>%
+    mutate(sample=sample_size) %>%
+    mutate(percent_missing=(sample/nrow(traitsDF))*100)
+ 
+  
+}
+
+lapply_with_error <- function(X,FUN,...){    
+  lapply(X, function(x, ...) tryCatch(FUN(x, ...),
+                                      error=function(e) NULL))
+}
+
+# adding the lapply with error
+# allows to 'skip'
+# the possibility when something is left out
+# that has a level not in the model
+# e.g., primary lifestyle this happens
+random_sampling_results <- bind_rows(lapply_with_error(c(1:60), random_sampling_function))
+
+# quick crack at a potential visualization
+# not sure what I'm doing
+ggplot(random_sampling_results, aes(x=percent_missing, y=predicted_ESW))+
+  geom_point()
+
+# that didn't look too good...
+
+# try again?
+random_sampling_results %>%
+  mutate(percent_missing=as.numeric(percent_missing)) %>%
+  nest(data = -percent_missing) %>% 
+  mutate(
+    fit = map(data, ~ lm(predicted_ESW ~ observed_ESW, data = .x)),
+    tidied = map(fit, tidy),
+    glanced=map(fit, glance)
+  ) %>% 
+  unnest(glanced) %>%
+  ggplot(., aes(x=percent_missing, y=r.squared))+
+  geom_jitter()
+
+
+# so that kind of makes sense
+# but with the lapply with error....
+# by the time you have the majority of species being 'left-out'
+# then the levels are gone so it isn't working.
+# just as a test case I'm gonna copy and paste the above
+# but just use body mass (as we know we have this for all species and don't have to worry about levels)
+# but how many species can we leave out at a time and still get a good predictive accuracy of the model?
+random_sampling_function_body <- function(sample_size){
+  
+  boot_fun <- function(draw_number){
+    
+    dat_filtered <- traitsDF %>%
+      sample_n(nrow(traitsDF)-sample_size)
+    
+    mod <- lm(ESW ~ log(Mass),
+              data=dat_filtered)
+    
+    new_dat <- traitsDF %>%
+      dplyr::filter(!Species %in% dat_filtered$Species)
+    
+    predicted_ESW <- predict(mod, new_dat)
+    
+    loo_summary <- data.frame(Species=new_dat$Species,
+                              observed_ESW=new_dat$ESW,
+                              predicted_ESW=predicted_ESW) %>%
+      mutate(draw=draw_number)
+    
+    return(loo_summary)
+    
+  }
+  
+  boot_results <- bind_rows(lapply(c(1:50), boot_fun)) %>%
+    mutate(sample=sample_size) %>%
+    mutate(percent_missing=(sample/nrow(traitsDF))*100)
+  
+  
+}
+
+# adding the lapply with error
+# allows to 'skip'
+# the possibility when something is left out
+# that has a level not in the model
+# e.g., primary lifestyle this happens
+random_sampling_results <- bind_rows(lapply_with_error(c(1:60), random_sampling_function_body))
+
+# try again?
+random_sampling_results %>%
+  mutate(percent_missing=as.numeric(percent_missing)) %>%
+  nest(data = -percent_missing) %>% 
+  mutate(
+    fit = map(data, ~ lm(predicted_ESW ~ observed_ESW, data = .x)),
+    tidied = map(fit, tidy),
+    glanced=map(fit, glance)
+  ) %>% 
+  unnest(glanced) %>%
+  ggplot(., aes(x=percent_missing, y=r.squared))+
+  geom_jitter()
+
+# Ah, that's more what I was expecting!
+# Kinda cool... I think?
+
+
+
 #75%
 
 ### det covariates ####
